96741548a9e5180df517d6a4ab15b860f89d64c2
Justin Minsk <justin.minsk@gmail.com>
2017-10-25 12:46:04 -0400

    Added class work on jane austen
    Signed-off-by: Justin Minsk <justin.minsk@gmail.com>

---
+++ Text/Test_text.R
@@ -0,0 +1,73 @@
+library(tidytext)
+library(tm)
+library(wordcloud)
+library(stringr)
+library(janeaustenr)
+
+#--------------
+
+lines <- scan("lyrics.txt", what = character(), sep = '\n')
+#take all of the lyrics from the text doc and create an array by line
+
+lines_df <- data_frame(line = 1:119, text = lines)
+#make a data frame with na index and each line
+
+words_df <- lines_df%>%
+  unnest_tokens(word, text)
+#store the words and maintain what line they where on
+
+words_df <- words_df%>%
+  filter(!(word %in% stop_words$word))
+#takes out all of the common words form the list of words form the songs
+
+words_free <- words_df%>%
+  group_by(word)%>%
+  summarise(count = n())
+#get a count of the words
+
+wordcloud(words_free$word, words_free$count, colors =  brewer.pal(5, "Dark2"))
+#create a wordcloud
+
+#-------------
+
+sns <- austen_books()
+#bring in all of jane austen's books
+
+sns <- sns%>%
+  filter(book == "Sense & Sensibility")
+#filter to just get Sense and Sensibility text
+
+sns$book <- as.character(sns$book)
+#un factor the book column
+
+str_detect(sns$text, "^CHAPTER")
+#make a bool list with true for anyhting begining with CHAPTER
+
+sns <- sns%>%
+  filter(!str_detect(sns$text, "^CHAPTER"))
+#filter out all of the CHAPTER lines
+
+sns[1:11, ]
+#see only row 1 -11 all columns
+
+sns <- sns[12:12574, ]
+#make sns start after the title 
+
+sns <- sns[1:12560, ]
+#get rid of the end
+
+words_df <- sns%>%
+  unnest_tokens(word, text)
+#split the lines into words
+
+words_df <- words_df%>%
+  filter(!(word %in% stop_words$word))
+#get rid of common words that are not unique (the, a, etc.)
+
+words_free <- words_df%>%
+  group_by(word)%>%
+  summarise(count = n())
+#make a count of the word
+
+wordcloud(words_free$word, words_free$count, min.freq = 25)
+#make a word cloud
